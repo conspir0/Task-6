acf3934d9b576aeadf70ca330c864aa78ca2a0df
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-02-18 09:23:17 +0100

    Add handleClick, handleChange function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 class App extends React.Component {
   state = {
-    username: "",
+    userName: "",
     password: "",
+    text: ""
+  };
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+//    console.log(this.state);
+  };
+
+  handleClick = () => {
+    this.setState({
+      text: this.state.text
+    });
   };
 
   render() {
@@ -11,25 +24,26 @@ class App extends React.Component {
       <div className="popup">
         <p className="popup-title">Are you a Mountain Knight?</p>
         <input
-          onChange={this.onChange}
+          onChange={this.handleChange}
           className="popup-email"
           type="email"
-          name="username"
+          name="userName"
           placeholder="Email"
         />
         <input
-          onChange={this.onChange}
+          onChange={this.handleChange}
           className="popup-password"
           type="password"
           name="password"
           placeholder="Password"
         />
         <input
-          onClick={this.onChange}
+          onClick={this.handleClick}
           className="popup-button"
           type="submit"
           value="LOG IN"
         />
+        <p className="popup-title">{this.state.text}</p>
       </div>
     );
   }
