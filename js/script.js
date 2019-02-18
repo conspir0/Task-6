08ac66205d201822d7228114ac3d32a2f28e7ee1
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-02-18 01:13:32 +0100

    Add Mixin - popupInput, Popup

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@ button.addEventListener('click', () => {
     button.classList.toggle('close');
     menu.classList.toggle('show');
 });
+
+// Popup
+//-----------------------------------------------------
+const showModal = function(event){
+    event.preventDefault();
+    document.querySelector('#modal-overlay').classList.add('show');
+};
+	
+const modalLinks = document.querySelector('.show-modal');
+	
+    modalLinks.addEventListener('click', showModal);
+
+const hideModal = function(event){
+    event.preventDefault();
+    document.querySelector('#modal-overlay').classList.remove('show');
+}; 
+	
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+	
+const modals = document.querySelector('.modal');
+	
+modals.addEventListener('click', function(event){
+    event.stopPropagation();
+});
